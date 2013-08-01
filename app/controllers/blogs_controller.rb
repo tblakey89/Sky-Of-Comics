b7472e211a63393d2659ba3c74bb7219d3cb8402
tblakey89 <tomblakey89@googlemail.com>
2013-08-01 15:57:14 +0100

    add view show and new template for blog

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -6,15 +6,24 @@ class BlogsController < ApplicationController
   end
 
   def show
-
+    @blog = Blog.find(params[ :id])
+    @commentable = @blog
+    @comments = @commentable.comments
+    @comment = Comment.new
   end
 
   def new
-
+    @blog = Blog.new
   end
 
   def create
-
+    @blog = current_user.blogs.build(params[:blog])
+    if @blog.save
+      flash[:success] = "Blog created"
+      redirect_to @blog
+    else
+      render 'new'
+    end
   end
 
   def update
